901641f45fde7aa83e76fa6b6cb9cf122ebd6518
Joachim <joachim.martine@eleves.enpc.fr>
2024-03-11 16:29:09 +0100

    rajouts de commentaires

--- Inpainter.h
+++ Inpainter.h
@@ -11,26 +11,26 @@ class Inpainter{
     int patch_size;
     Img working_image;
     Img working_mask;
-    Img front;
-    Img confidence;
+    Img front; //stockage frontière
+    Img confidence; //à changer de type, passer sur un tableau ? sauf si changement de type d'image
     Img data;
     Img priority;
 public:
-    Inpainter(Img im, Img msk, int patch_size = 9);
-    Img inpaint();
-    bool validate_inputs();
-    void plot_image();
-    void initialize_attributes();
-    void find_front();
-    void update_priority();
-    void update_confidence();
-    void update_data();
-    Img calc_normal_matrix();
-    Img calc_gradient_matrix();
-    Crds find_highest_priority_pixel();
-    Img find_source_patch(Crds target_pixel);
-    void update_image(Crds target_pixel, Img source_patch);
-    Img get_patch(Crds point);
+    Inpainter(Img im, Img msk, int patch_size = 9); //commentaires
+    Img inpaint(); //démarre processus
+    bool validate_inputs(); //cheque taille des éléments
+    void plot_image(); //gère affichage
+    void initialize_attributes(); //initialiser attribus,( confiance...)
+    void find_front(); //recherche de la frontière à partir du masque
+    void update_priority(); //calcul des priorités
+    void update_confidence(); // mise à jour valeurs confiance
+    void update_data(); //terme données
+    Img calc_normal_matrix(); // calcul de la normale ?
+    Img calc_gradient_matrix(); // calcul du gradient
+    Crds find_highest_priority_pixel(); //cherche pixel plus grande prioeiré
+    Img find_source_patch(Crds target_pixel); //cerche patch idéal
+    void update_image(Crds target_pixel, Img source_patch); //copie coller du patch
+    Img get_patch(Crds point); //récupère l'image à partir du patch
     double calc_patch_difference(Img im, Img target_patch, Img source_patch);
     bool finished();
 };
