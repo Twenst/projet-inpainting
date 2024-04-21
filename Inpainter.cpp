0963cd083a5ed845653da7131285ee8c7fe7d736
Zoomiz <teogspam@gmail.com>
2024-04-21 21:36:43 +0200

    fonction Téodora finies v1

--- Inpainter.cpp
+++ Inpainter.cpp
@@ -165,7 +165,7 @@ Img Inpainter::find_source_patch(Crds target_pixel){
     double best_match_diff = std::numeric_limits<double>::infinity();
 
     for (int i=0;i<= width - patch_size;i++){
-        for (int j=4;j<height - patch_size; j++){
+        for (int j=0;j<height - patch_size; j++){
             Img mask_patch = working_mask.getSubImage(i,j, patch_size,patch_size);
 
             bool not_empty = true ;
@@ -182,6 +182,7 @@ Img Inpainter::find_source_patch(Crds target_pixel){
 
             }
 
+            //s'il n'en contient pas on regarde sa différence par rapport au patch que l'on veut remplir et on remplace best_match si nécessaire
             if (not_empty){
                 Img source_patch=working_image.getSubImage(i,j, patch_size,patch_size);
                 double diff = calc_patch_difference(working_image, target_patch, source_patch);
@@ -197,6 +198,32 @@ Img Inpainter::find_source_patch(Crds target_pixel){
 }
 
 void Inpainter::update_image(Crds target_pixel, Img source_patch){
+    //on récupère le coin en haut à gauche du patch que l'on veut remplir
+    int half_patch_size= (patch_size-1)/2;
+    const int width=image.width(); //dans le code python il fait sur working image mais en vrai c'est pareil
+    const int height=image.height();
+    //on veut renvoyer le patch centré en point mais getSubImage prend en argument le pixel en haut à gauche
+    assert((target_pixel.x() - half_patch_size) > 0 && (target_pixel.y() - half_patch_size) > 0 &&
+           (target_pixel.x() + half_patch_size) < width && (target_pixel.y() + half_patch_size) < height);
+    int coord_x = target_pixel.x() - half_patch_size;
+    int coord_y = target_pixel.y() - half_patch_size;
+    int confidence_target_pixel = confidence.data()[coord_x][coord_y]; //valeur de confiance du pixel autour duquel on remplace
+
+
+    //on ne va modifier que les pixels qui sont blancs sur working_mask
+    //dans le code python il modifie aussi le pixel en haut à gauche mais ça me semble inutile donc je l'ai pas fait
+    for (int a=0; a<=patch_size;a++){
+        for (int b=0; b<=patch_size;b++){
+            if (working_mask.data()[coord_x+a][coord_y+b] == WHITE){
+                working_mask.data()[coord_x+a][coord_y+b] = BLACK; //on modifie leur couleur sur working_mask car ils sont remplis
+                working_image.data()[coord_x+a][coord_y+b] = source_patch.data()[a][b]; //on met la vraie couleur sur working_image
+                confidence.data()[coord_x+a][coord_y+b] = confidence_target_pixel; //on met la bonne valeur de confiance
+
+            }
+
+        }
+
+    }
 }
 
 Img Inpainter::get_patch(Crds point){
@@ -211,6 +238,9 @@ Img Inpainter::get_patch(Crds point){
 
     Img patch= working_image.getSubImage(coord_x,coord_y,patch_size,patch_size);
     return patch;
+    //contrairement à la fonction du code python on ne peut pas avoir de patch centré en un point trop proche du bord à voir si ça pose pb
+    //dans python la 1ère coordonnée correspond à la hauteur du pixel alors qu'ici x est l'axe de la largeur
+    //dans le code python dans get_patch patch[0] contient [y_min,y_max] et patch[1] contient [x_min,x_max] alors qu'ici on a une image
 }
 
 double Inpainter::calc_patch_difference(Img im, Img target_patch, Img source_patch){
