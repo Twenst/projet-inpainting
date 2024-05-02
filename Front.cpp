778a39baded8b09ecc1fb68d7c757fad7c0c433a
Titouan <titouan@gaelane.org>
2024-05-02 15:31:33 +0200

    debut utils

--- Front.cpp
+++ Front.cpp
@@ -27,7 +27,7 @@ void Front::updateFront(const ImgPixel& Img, list<Pixel> list_pixels)
     // demander à Felix la diff entre define et update
 }
 
-void Front::clearSides(const ImgPixel& Img)
+void Front::clearSides(const ImgPixel& Img) //enleve les pixels de la frontiere qui sont sur le bord de l'image
 {
     int w = Img.width(), h = Img.height();
 
@@ -71,12 +71,4 @@ void Front::display()
     {
         drawPoint(i->getX(), i->getY(), BLACK);
     }
-}
-
-bool selectZone(int& x1,int& y1,int& x2,int& y2)
-{
-    return(getMouse(x1, y1)==1 && getMouse(x2,y2)==1);
-    /* demande deux clics gauches, remplit x1, y1, x2 et y2 avec les coordonnées des clics et retourne True
-     * En cas de clic droit, retourne False
-     */
 }
---
+++ utils.cpp
@@ -0,0 +1,32 @@
+#include "utils.h"
+
+double absf(double f)
+{
+    return (f > 0) ? f : -f;
+}
+
+bool selectZone(int& x1, int& y1, int& x2, int& y2)
+{
+    // demande deux clics gauches, remplit x1, y1, x2 et y2 avec les coordonnées des clics et retourne True
+    // En cas de clic droit, retourne False
+    
+    return(getMouse(x1, y1)==1 and getMouse(x2,y2)==1);
+}
+
+void toImageByte(ImgPixel Img, ImgByte Ib)
+{
+
+}
+
+void computeGradientNormal(double gradient[2], double normal[2], ImgPixel Img, Pixel p, ImgByte ImgB)
+{
+    
+}
+
+double computeData(double gradient[2],double normal[2]) // cd code
+{
+    double d = gradient[0]*normal[0];
+    d += gradient[1]*normal[1];
+
+    return double(absf(d)/256);
+}
