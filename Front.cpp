2ddb2796fa9b343b5bc05aa728aff89867e21b53
Titouan <titouan@gaelane.org>
2024-05-02 14:48:19 +0200

    front

--- Front.cpp
+++ Front.cpp
@@ -6,27 +6,72 @@ Front::Front()
 {
 }
 
-Front::Front(std::list<Pixel> pxs)
+Front::Front(list<Pixel> pxs)
 {
     pixels=pxs;
 }
 
-void Front::defineFront(const ImgPixel& Img, std::list<Pixel> list_pixels)
+void Front::defineFront(const ImgPixel& Img, list<Pixel> list_pixels)
 {
     int w = Img.width(), h = Img.height();
 
-    std::list<Pixel>::iterator iter = pixels.end();
+    list<Pixel>::iterator iter = pixels.end();
 
-    for (std::list<Pixel>::iterator i = list_pixels.begin(); i != list_pixels.end(); i++)
+    for (list<Pixel>::iterator i = list_pixels.begin(); i != list_pixels.end(); ++i)
         if (i->getFilled())
             pixels.insert(iter,*i);
 }
 
-void updateFront(const ImgPixel& Img, std::list<Pixel> list_pixels)
+void Front::updateFront(const ImgPixel& Img, list<Pixel> list_pixels)
+{
+    // demander à Felix la diff entre define et update
+}
+
+void Front::clearSides(const ImgPixel& Img)
+{
+    int w = Img.width(), h = Img.height();
+
+    for (list<Pixel>::iterator i = pixels.begin(); i != pixels.end(); ++i)
+    {
+        int x = i->getX(), y = i->getY();
+        if (x == 0 or y == 0 or x >= w-1 or y >= h-1)
+        {
+            pixels.remove(*i);
+        }
+    }
+}
+
+Pixel Front::pixelMaxPriority() // on peut pas mettre cette fonction en const à cause de l'iterator
+{
+    Pixel p_max = pixels.front();
+    
+    for (list<Pixel>::iterator i = pixels.begin(); i != pixels.end(); ++i)
+    {
+        if (p_max.getPriority() < i->getPriority())
+        {
+            p_max = *i;
+        }
+    }
+    return p_max;
+} 
+
+void Front::updateData(ImgPixel& Img)
 {
 
 }
 
+bool Front::isEmpty()
+{
+    return pixels.empty();
+}
+
+void Front::display()
+{
+    for (list<Pixel>::iterator i = pixels.begin(); i != pixels.end(); ++i)
+    {
+        drawPoint(i->getX(), i->getY(), BLACK);
+    }
+}
 
 bool selectZone(int& x1,int& y1,int& x2,int& y2)
 {
