40014485df2ecf49cfe5d885bb23b471d834af62
Titouan <titouan@gaelane.org>
2024-05-06 15:03:49 +0200

    push

--- utils.h
+++ utils.h
@@ -13,6 +13,7 @@ int max(int a, int b);
 bool selectZone(int& x1,int& y1,int& x2,int& y2);           // Permet de sélectionner un rectangle sur l'image
 void toImageByte(ImgPixel Img, ImgByte Ib);
 void computeGradientNormal(double gradient[2], double normal[2], ImgPixel Img, Pixel p, ImgByte ImgB);    // Calcule le gradient et le vecteur normal au pixel p
-void computeGradientNormalv2(double gradient[2], double normal[2], ImgPixel Img, Pixel p, ImgByte ImgB);
+void computeGradientNormalUsingByte(double gradient[2], double normal[2], ImgPixel Img, Pixel p, ImgByte ImgB);
+void computeGradientNormalUsingColor(double gradient[2], double normal[2], ImgPixel Img, Pixel p, ImgByte ImgB);
 double computeData(double gradient[2],double normal[2]);                                                  // Calcule le terme de donnée à partir du gradient et de la normale
 
